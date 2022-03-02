b6dfcebc1c12dbf26ee879ad69f79947dbb680c2
Kerolen Rodrigues <kerolen_karina23@hotmail.com>
2022-03-02 14:02:57 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/kerolenrodrigues/projeto/repositories/ItemPedidoRepository.java
@@ -0,0 +1,12 @@
+package com.kerolenrodrigues.projeto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kerolenrodrigues.projeto.domain.Cidade;
+import com.kerolenrodrigues.projeto.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
