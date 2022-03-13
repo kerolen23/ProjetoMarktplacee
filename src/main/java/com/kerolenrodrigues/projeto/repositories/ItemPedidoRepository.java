c69de23ab9bf47593bcfa353264b709664fd176e
Kerolen Rodrigues <kerolen_karina23@hotmail.com>
2022-03-13 10:11:49 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/kerolenrodrigues/projeto/repositories/ItemPedidoRepository.java
+++ src/main/java/com/kerolenrodrigues/projeto/repositories/ItemPedidoRepository.java
@@ -2,8 +2,6 @@ package com.kerolenrodrigues.projeto.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.kerolenrodrigues.projeto.domain.Cidade;
 import com.kerolenrodrigues.projeto.domain.ItemPedido;
 
 @Repository
