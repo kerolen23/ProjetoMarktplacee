d8f668872c3870a0b111abd18bb249ff04338920
Kerolen Rodrigues <kerolen_karina23@hotmail.com>
2022-03-15 14:04:25 -0300

    Personalizando o retorno da validação

--- src/main/java/com/kerolenrodrigues/projeto/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/kerolenrodrigues/projeto/resources/exception/ResourceExceptionHandler.java
@@ -3,8 +3,11 @@ package com.kerolenrodrigues.projeto.resources.exception;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentConversionNotSupportedException;
 
 import com.kerolenrodrigues.projeto.services.exceptions.DataIntegrityViolationException;
 import com.kerolenrodrigues.projeto.services.exceptions.ObjectNotFoundException;
@@ -25,6 +28,18 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityViolationException e, HttpServletRequest request) {
 		
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	 }
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> Validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Erro de validação", System.currentTimeMillis());
+		
+		for(FieldError x :  e.getBindingResult().getFieldErrors()) {
+		    err.addError(x.getField(), x.getDefaultMessage());	
+		}
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	 }
