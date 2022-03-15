c81b8fbeb97a216f5048668292572d0b2dea56f2
Kerolen Rodrigues <kerolen_karina23@hotmail.com>
2022-03-15 09:25:31 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/kerolenrodrigues/projeto/services/CategoriaService.java
+++ src/main/java/com/kerolenrodrigues/projeto/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.kerolenrodrigues.projeto.domain.Categoria;
+import com.kerolenrodrigues.projeto.dto.CategoriaDTO;
 import com.kerolenrodrigues.projeto.repositories.CategoriaRepository;
 import com.kerolenrodrigues.projeto.services.exceptions.ObjectNotFoundException;
 
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+				return new Categoria(objDto.getId(), objDto.getNome()); 
 	}
 }
