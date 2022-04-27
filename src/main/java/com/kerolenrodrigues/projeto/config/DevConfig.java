73c1eab6953314062f41bc1f9d3126de9d5b41d9
Kerolen Rodrigues <kerolen_karina23@hotmail.com>
2022-04-27 17:39:14 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/kerolenrodrigues/projeto/config/DevConfig.java
+++ src/main/java/com/kerolenrodrigues/projeto/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.kerolenrodrigues.projeto.services.DBService;
+import com.kerolenrodrigues.projeto.services.EmailService;
+import com.kerolenrodrigues.projeto.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
